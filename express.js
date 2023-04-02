fcafcbf401865580fd8165120ca0497e92cfa121
giuliana <giuliana.torres05@gmail.com>
2023-04-01 23:17:21 -0600

    Added to functions to catch a req query that is not a number.

--- express.js
+++ express.js
@@ -1,34 +1,81 @@
 const express = require("express");
 const app = express();
 const process = require('process')
-const oper = require("./operations");
+const Operations = require("./operations");
+const ExpressError = require("./expressError");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    return res.send("Hey there!")
-})
+app.get('/mean', (req, res, next) => {
+    if (!req.query.nums) {
+        throw new ExpressError("Nums are required.", 400);
+    };
+
+    let numArr = req.query.nums.split(',');
+    let x = new Operations(numArr);
+
+    let invalidNum = numArr.find(num => isNaN(num) == true);
+    if (invalidNum) {
+        throw new ExpressError(`${invalidNum} is not a valid number.`, 400);
+    };
+
+    return res.json(x.mean());
+});
 
 
-app.get('/mean', (req, res) => {
-    let x = new oper.Operations(req.query.nums)
-    return res.json(x.mean())
-})
+app.get('/median', (req, res, next) => {
+    if (!req.query.nums) {
+        throw new ExpressError("Nums are required.", 400);
+    };
 
+    let numArr = req.query.nums.split(',');
+    let x = new Operations(numArr);
 
-app.get('/median', (req, res) => {
-    let x = new oper.Operations(req.query.nums)
+    let invalidNum = numArr.find(num => isNaN(num) == true);
+    if (invalidNum) {
+        throw new ExpressError(`${invalidNum} is not a valid number.`, 400);
+    };
+    
     return res.json(x.median())
-})
+});
 
 
-app.get('/mode', (req, res) => {
-    let x = new oper.Operations(req.query.nums)
+app.get('/mode', (req, res, next) => {
+    if (!req.query.nums) {
+        throw new ExpressError("Nums are required.", 400);
+    };
+
+    let numArr = req.query.nums.split(',');
+    let x = new Operations(numArr);
+
+    let invalidNum = numArr.find(num => isNaN(num) == true);
+    if (invalidNum) {
+        throw new ExpressError(`${invalidNum} is not a valid number.`, 400);
+    };
+
     return res.json(x.mode())
-})
+});
+
+
+// this runs if no routes match
+app.use((req, res, next) => {
+    const err = new ExpressError("Page Not Found", 404);
+    return next(err);
+});
+
+
+// displays custom error msg & sets status code
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+
+    return res.json({
+        error: err,
+        message: err.msg
+    });
+});
 
 
 app.listen(3000, function(){
     console.log('App on port 3000');
-  })
+});
